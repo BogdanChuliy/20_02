8a10345ea40da245bb1d386b3458547957ce8239
Devvision <for.a.kvadrat@gmail.com>
2017-02-20 18:12:07 +0200

    init commit

---
+++ Basi_20_02_2017_lesson_06/src/Main.java
@@ -0,0 +1,46 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int array [] = {12,5,3,434,35,34};
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+		for(int iter : array){
+			System.out.println(iter);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+		}
+
+}
